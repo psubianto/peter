82ad9074e91c777ccf35da533d37b2df670f1ff1
Peter Subianto <psubianto@metroalert.com>
2015-01-13 10:55:21 -0500

    Update cordova, update phonegap, platform remove android, platform add android

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Repos\peter\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Repos\peter\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Repos\peter\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Repos\peter\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Repos\peter\platforms\android\ant-build\classes.dex \
-C:\Repos\peter\platforms\android\CordovaLib\ant-build\classes.jar \
